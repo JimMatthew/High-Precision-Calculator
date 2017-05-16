3fb1a5ba8d664d5076eaed1c93fb0730db64c3df
JimMatthew <jameslindstrom2@gmail.com>
2017-05-16 14:01:36 -0500

    Add files via upload

---
+++ calculatorGui.java
@@ -0,0 +1,9 @@
+
+public interface calculatorGui {
+
+	public void setRunningCalcDisplay(String string);
+	
+	public void setCurrentTotalDisplay(String string);
+	
+	public void setCalculatorController(calculatorController controller);
+}
