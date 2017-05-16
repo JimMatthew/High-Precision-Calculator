f76356ee82563d14b4fd83bf8c67f40c1d3889bc
JimMatthew <jameslindstrom2@gmail.com>
2017-05-16 14:08:26 -0500

    Revert "Cleanup"

--- calculatorGui.java
+++
@@ -1,9 +0,0 @@
-
-public interface calculatorGui {
-
-	public void setRunningCalcDisplay(String string);
-	
-	public void setCurrentTotalDisplay(String string);
-	
-	public void setCalculatorController(calculatorController controller);
-}
